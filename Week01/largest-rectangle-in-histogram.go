53ec83a67378222f06dae9fda5ffbf41d1873b48
zcc <438088708@qq.com>
2020-06-12 09:55:56 +0800

    2

--- Week01/largest-rectangle-in-histogram.go
+++ Week01/largest-rectangle-in-histogram.go
@@ -3,40 +3,26 @@ package main
 import "fmt"
 
 
-//func largestRectangleArea(heights []int) int {
-//	max := 0
-//	if len(heights) < 1 {
-//		return 0
-//	}else if len(heights) == 1 {
-//		return heights[0]*1
-//	}
-//
-//	for i:=0;i<=len(heights)-2 ;i++  {
-//		for j:=i;j<=len(heights)-1;j++  {
-//			area := 0
-//			if heights[i]>heights[j]{
-//				area = heights[j]*(j-i+1)
-//			}else{
-//				area = heights[i]*(j-i+1)
-//			}
-//			if area > max {
-//				max = area
-//			}
-//		}
-//	}
-//	return max
-//}
-
-func largestRectangleArea(heights []int) int {
+/**
+暴力法一：枚举宽
+TODO
+*/
+func largestRectangleArea2(heights []int) int {
+	if len(heights) < 1 {
+		return 0
+	}else if len(heights) == 1 {
+		return heights[0]*1
+	}
 	max := 0
-	for left:=0;left<len(heights)-1 ;left++  {
-		for right:=left; right<len(heights)-1;right++  {
+	for left:=0;left<=len(heights)-1;left++  {
+		for right:=left+1; right<=len(heights)-1;right++  {
 			area := 0
 			if heights[left]>heights[right]{
 				area = heights[right]*(right-left+1)
 			}else{
 				area = heights[left]*(right-left+1)
 			}
+			fmt.Println(left,right,heights[left],heights[right],area)
 			if area > max {
 				max = area
 			}
@@ -45,9 +31,72 @@ func largestRectangleArea(heights []int) int {
 	return max
 }
 
+/**
+暴力法2：枚举高
+ */
+func largestRectangleArea1(heights []int) int {
+	max := 0
+	if len(heights) < 1 {
+		return 0
+	}else if len(heights) == 1 {
+		return heights[0]*1
+	}
+
+	for i:=0;i<=len(heights)-1;i++  {
+		height := heights[i]
+		left := i
+		right := i
+		for left -1 >= 0 && heights[left-1] >= height {
+			left --
+		}
+		for right + 1 < len(heights) && heights[right+1] >= height  {
+			right ++
+		}
+		area := (right-left+1)*height
+		if area > max {
+			max = area
+		}
+	}
+	return max
+}
+
+
+/**
+单调栈
+利用一个栈实现
+这个栈用来存储最大高度得下标
+遍历数组
+
+ */
+func largestRectangleArea(heights []int) int {
+	res := 0
+	heights = append([]int{-2},heights...)
+	heights = append(heights,-1)
+	n := len(heights)
+	//递增栈 记录最大高度得下标
+	s := make([]int,1,n)
+	i := 1
+	for i < n {
+		if heights[s[len(s)-1]] < heights[i] {
+			s = append(s,i)
+			i++
+			continue
+		}
+		res = max(res,heights[s[len(s)-1]]*(i-s[len(s)-2]-1))
+		s = s[:len(s)-1]
+	}
+	return res
+}
+
+func max(a,b int)int{
+	if a>b{return a}
+	return b
+}
+
 func main()  {
 	fmt.Println(largestRectangleArea([]int{2,1,5,6,2,3}))
 	fmt.Println(largestRectangleArea([]int{1,1}))
 	fmt.Println(largestRectangleArea([]int{5,1}))
-	fmt.Println(largestRectangleArea([]int{}))
+	fmt.Println(largestRectangleArea([]int{1}))
+	fmt.Println(largestRectangleArea([]int{0,9}))
 }
