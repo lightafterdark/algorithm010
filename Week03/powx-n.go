538bcf61c9ec7d6ca74355602260689bcd68125e
zcc <438088708@qq.com>
2020-06-27 23:35:00 +0800

    work

---
+++ Week03/powx-n.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1.0
+	}else if n == 1 {
+		return x
+	}else if n < 0 {
+		return 1.0/myPow(x,-n)
+	}
+
+	subRes := myPow(x,n/2)
+	if n % 2 == 1 {
+		return subRes*subRes*x
+	}else{
+		return subRes*subRes
+	}
+}
+
+func main()  {
+	fmt.Println(myPow(2.0,1))
+	fmt.Println(myPow(2.0,3))
+	fmt.Println(myPow(2.0,0))
+	fmt.Println(myPow(2.0,5))
+}
