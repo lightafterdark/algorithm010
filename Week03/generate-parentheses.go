c8cf4d7c5cbbf74a1e31b5ffc48ec25c2277eca9
zcc <438088708@qq.com>
2020-06-27 22:14:27 +0800

    work

---
+++ Week03/generate-parentheses.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func generateParenthesis(n int) []string {
+	res := make([]string,0)
+	generate(0,0,n,"",&res)
+	return res
+}
+
+func generate(left,right,n int,s string,res *[]string){
+	if left == n && right == n {
+		*res = append(*res,s)
+	}
+
+	if left < n {
+		generate(left+1,right,n,s+"(",res)
+	}
+
+	if left > right {
+		generate(left,right+1,n,s+")",res)
+	}
+}
+
+func main()  {
+	fmt.Println(generateParenthesis(3))
+}
